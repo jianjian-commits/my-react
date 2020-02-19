97e6fcb781538be2eddc4866cdd2bdcc1bf1eda5
liuyang <liuyang@wctsoft.com>
2020-02-19 15:33:25 +0800

    merge

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 export const serverHost =
-  process.env.REACT_APP_HOST_IP || "http://192.168.3.127";
-export const port = process.env.REACT_APP_HOST_PORT || 8080;
+  process.env.REACT_APP_HOST_IP || "http://192.168.3.106";
+export const port = process.env.REACT_APP_HOST_PORT || 9080;
