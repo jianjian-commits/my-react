5341d298555d450aab86db3e02194a7f2808f72e
liuyang <liuyang@wctsoft.com>
2020-02-26 18:10:21 +0800

    master url

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const serverHost =
-  process.env.REACT_APP_HOST_IP || "http://192.168.3.30";
-export const port = process.env.REACT_APP_HOST_PORT || 8080;
+  process.env.REACT_APP_HOST_IP || "http://192.168.3.106";
+export const port = process.env.REACT_APP_HOST_PORT || 9080;
 export const env = "REMOTE"; // REMOTE/LOCAL
