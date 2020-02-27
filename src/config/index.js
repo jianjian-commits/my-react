153d8add6567bf8fc25130847d57b27e8343ec48
吴凯津 <wukaijin@wctsoft.com>
2020-02-27 21:06:04 +0800

    env

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,2 @@
-export const HOST_IP =
-  process.env.REACT_APP_HOST_IP || "http://192.168.3.103:9080";
+export const HOST_IP = "" || process.env.REACT_APP_HOST_IP;
 export const env = "REMOTE"; // REMOTE/LOCAL
