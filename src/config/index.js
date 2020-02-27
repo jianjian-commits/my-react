546067e0116df0e912060e759993040f4a70745d
jgchenu <jgchenu@foxmail.com>
2020-02-27 15:52:26 +0800

    联调分组接口

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,9 @@
+// export const serverHost =
+//   process.env.REACT_APP_HOST_IP || "http://192.168.3.106";
+// export const port = process.env.REACT_APP_HOST_PORT || 9080;
+// export const env = "REMOTE"; // REMOTE/LOCAL
+
 export const serverHost =
-  process.env.REACT_APP_HOST_IP || "http://192.168.3.106";
-export const port = process.env.REACT_APP_HOST_PORT || 9080;
-export const env = "REMOTE"; // REMOTE/LOCAL
+  process.env.REACT_APP_HOST_IP || "http://192.168.3.126";
+export const port = process.env.REACT_APP_HOST_PORT || 8080;
+export const env = "REMOTE";
--- src/utils/request.js
+++ src/utils/request.js
@@ -7,43 +7,20 @@ const baseURL = `${serverHost}${port ? ":" + port : ""}`;
 async function request(url, params = {}) {
   const headers = params.headers || {};
   headers["Content-Type"] = "application/json" || params.contentType;
-  try {
-    const res = await axios({
-      url: `${baseURL}${url}`,
-      headers,
-      data: params.data || {},
-      params: params.params || {},
-      method: params.method || "GET"
-    });
-    console.log("fetch options", params);
-    console.log("fetch res", res);
-    return res.data;
-  } catch (error) {
-    return console.log(error);
-  }
+  // try {
+  const res = await axios({
+    url: `${baseURL}${url}`,
+    headers,
+    data: params.data || {},
+    params: params.params || {},
+    method: params.method || "GET"
+  });
+  console.log("fetch options", params);
+  console.log("fetch res", res);
+  return res.data;
+  // } catch (error) {
+  //   return console.log(error);
+  // }
 }
 
 export default request;
-
-// import Axios from "axios";
-// import { serverHost, port } from "./config";
-
-// // Axios.defaults.headers.post["content-type"] = "application/json";
-// // 创建一个新的实例
-// export let r = Axios.create({
-//   baseURL: `${serverHost}${port ? ":" + port : ""}`
-// });
-// r.interceptors.response.use(
-//   response => response.data,
-//   error => Promise.reject(error)
-// );
-
-// let request = function(url = "", options = {}) {
-//   return r({
-//     url,
-//     method: "get", // method先给一个get请求
-//     ...options // options中有method就会覆盖,
-//   });
-// };
-
-// export default request;
