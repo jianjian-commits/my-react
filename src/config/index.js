33754788ffa1fa8330f213f1d812f7e39271ca69
jgchenu <jgchenu@foxmail.com>
2020-02-27 21:33:48 +0800

    不修改config

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,2 @@
-export const HOST_IP =
-  "http://192.168.3.162:8080" || process.env.REACT_APP_HOST_IP;
+export const HOST_IP = "" || process.env.REACT_APP_HOST_IP;
 export const env = "REMOTE"; // REMOTE/LOCAL
