480c2267653fddb96c5a42503fad91a473a60273
吴凯津 <wukaijin@wctsoft.com>
2020-03-02 14:28:20 +0800

    UNAUTHENTICATED handle

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,16 +1,28 @@
 import axios from "axios";
 import { HOST_IP } from "../config";
 
-const baseURL = `${HOST_IP}`;
-
 axios.defaults.withCredentials = true;
+axios.defaults.baseURL = `${HOST_IP}`;
+axios.defaults.headers.common["Content-Type"] = "application/json";
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    console.log(error.response);
+    if (
+      error.response.status === 401 &&
+      error.response.data.status === "UNAUTHENTICATED"
+    ) {
+      if (localStorage.getItem("id_token")) localStorage.removeItem("id_token");
+    }
+    return Promise.reject(error);
+  }
+);
 
 async function request(url, params = {}) {
   const headers = params.headers || {};
-  headers["Content-Type"] = "application/json" || params.contentType;
   const res = await axios({
-    url: `${baseURL}${url}`,
-    // timeout: params.timeout || 4000,
+    url,
     headers,
     data: params.data || {},
     params: params.params || {},
@@ -20,5 +32,4 @@ async function request(url, params = {}) {
   // console.log("fetch res", res);
   return res.data;
 }
-
 export default request;
