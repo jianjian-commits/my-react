c0e49d24f8e9d9218181b90dd5d7b4497e6d2147
吴凯津 <wukaijin@wctsoft.com>
2020-03-10 18:01:14 +0800

    auth

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,8 +1,15 @@
 export const SUPER_ADMINISTRATOR = "*:*:*:*:*";
+
 export const TEAM_MANAGEMENT = "?:?:?:?:TMV";
-export const PROFILE_MANAGEMENT_LIST =
-  "?:class#com.wctsoft.davinci.shiro.entity.SysRole:?:?:DR";
-export const TEAM_MANAGEMENT_LIST =
-  "?:class#com.wctsoft.davinci.shiro.entity.SysRole:?:?:DR";
+
+const PROFILE_MANAGEMENT_PREFIX =
+  "?:class#com.wctsoft.davinci.shiro.entity.SysRole:?:?:";
+export const PROFILE_MANAGEMENT_LIST = `${PROFILE_MANAGEMENT_PREFIX}DR`;
+export const PROFILE_MANAGEMENT_NEW = `${PROFILE_MANAGEMENT_PREFIX}DC`;
+
+const TEAM_MANAGEMENT_PREFIX =
+  "?:class#com.wctsoft.davinci.shiro.entity.SysRole:?:?:";
+export const TEAM_MANAGEMENT_LIST = `${TEAM_MANAGEMENT_PREFIX}DR`;
+export const TEAM_MANAGEMENT_NEW = `${TEAM_MANAGEMENT_PREFIX}DC`;
 
 export default {};
