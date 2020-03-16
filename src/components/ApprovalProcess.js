4e0b5badfb8d6c08f7254a07d7855bfcad869a1b
吴凯津 <wukaijin@wctsoft.com>
2020-03-16 12:41:41 +0800

    app inside authenrication

--- src/components/ApprovalProcess.js
+++ src/components/ApprovalProcess.js
@@ -5,6 +5,7 @@ import { PrivateRoute } from "./shared";
 import { useParams } from "react-router-dom";
 import { requestWithHeaders } from "../utils/request";
 import { Approval } from "componentized-process";
+import { APP_FORM_VISIABLED } from "../auth";
 
 export default () => {
   const { url } = useRouteMatch();
@@ -16,25 +17,30 @@ export default () => {
     appId,
     formId
   };
+  const auth = APP_FORM_VISIABLED(appId, formId);
+  const authOptions = {
+    type: "redirect",
+    to: "/app/list"
+  };
+  console.log(auth)
   return (
     <Switch>
       <PrivateRoute
         path={`${url}/list`}
         exact
+        auth={auth}
+        authOptions={authOptions}
+        authType={"redirect"}
         component={Approval.List}
         key={"approval/list"}
         options={{ config }}
       />
       <PrivateRoute
+        auth={auth}
+        authOptions={authOptions}
         path={`${url}/builder`}
         component={Approval.Builder}
         key={"approval/builder"}
-        options={{ config }}
-      />
-      <PrivateRoute
-        path={`${url}/log`}
-        component={Approval.Log}
-        key={"approval/log"}
         options={{ config }}
       />
     </Switch>
--- src/components/ProcessAuto.js
+++ src/components/ProcessAuto.js
@@ -5,6 +5,7 @@ import { PrivateRoute } from "./shared";
 import { useParams } from "react-router-dom";
 import { requestWithHeaders } from "../utils/request";
 import { Process } from "componentized-process";
+import { APP_FORM_VISIABLED } from "../auth";
 
 export default () => {
   const { url } = useRouteMatch();
@@ -19,23 +20,34 @@ export default () => {
       edit: 1
     }
   };
+  const auth = APP_FORM_VISIABLED(appId, formId);
+  const authOptions = {
+    type: "redirect",
+    to: "/app/list"
+  };
   return (
     <Switch>
       <PrivateRoute
         path={`${url}/list`}
         exact
+        auth={auth}
+        authOptions={authOptions}
         component={Process.List}
         key={"process/list"}
         options={{ config }}
       />
       <PrivateRoute
         path={`${url}/builder`}
+        auth={auth}
+        authOptions={authOptions}
         component={Process.Builder}
         key={"process/builder"}
         options={{ config }}
       />
       <PrivateRoute
         path={`${url}/log`}
+        auth={auth}
+        authOptions={authOptions}
         component={Process.Log}
         key={"process/log"}
         options={{ config }}
