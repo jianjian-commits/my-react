0264b0519eca1ac00f3f1b41b97c125509fa3c99
chenyinjun <tnsihy@foxmail.com>
2020-03-17 10:59:15 +0800

    分组详情接口调通

--- src/components/profileManagement/GroupDetail.js
+++ src/components/profileManagement/GroupDetail.js
@@ -1,58 +1,52 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
-import { Icon, Button, message, Checkbox } from "antd";
-import classes from "./profile.module.scss";
 import request from "../../utils/request";
-import { getBasicInfo, getAppManage, getOtherManage } from "./DetailModule";
+import { Icon, Button, message } from "antd";
+import {
+  BaseInfoModule,
+  AppManagerModule,
+  PermissionsModule
+  // SettingModule
+} from "./DetailModule";
 
-class GroupDetail extends Component {
+const Top = ({ disabled, enterDetail, handleDetail }) => (
+  <>
+    <Icon type="arrow-left" onClick={() => enterDetail()} />
+    <span style={{ textIndent: "10px" }}>分组</span>
+    {!disabled && (
+      <>
+        <Button
+          type="primary"
+          onClick={() => handleDetail()}
+          style={{ color: "#fff" }}
+        >
+          保存
+        </Button>
+        <Button onClick={() => enterDetail()}>取消</Button>
+      </>
+    )}
+  </>
+);
+
+class GroupDetail1 extends Component {
   constructor(props) {
     super(props);
     this.state = {
       baseInfoBo: {},
       appManagerBos: [],
-      teamVisible: {},
       permissions: {},
-      setting: {},
-      edit: false
+      settings: {},
+      editable: false
     };
-    this.action = props.action;
     this.roleId = props.roleId;
-    this.oldPermissionAllTrue = [];
     this.oldAppAllTrue = [];
-    this.oldTeamVisibleTrue = [];
+    this.oldPermissionAllTrue = [];
     this.onChange = this.onChange.bind(this);
-    this.inputEdit = this.inputEdit.bind(this);
   }
-
   componentDidMount() {
     this.getDetail();
   }
 
-  // 获取被选中的选项值value
-  getTrueValue(state, type) {
-    const result = [];
-    switch (type) {
-      case "permissions":
-        for (let key in state) {
-          state[key]
-            .filter(item => item.checked)
-            .map(v => result.push(v.value));
-        }
-        return result;
-      case "appManagerBos":
-        state.filter(item => item.checked).map(v => result.push(v.value));
-        return result;
-      case "teamVisible":
-        state.checked && result.push(state.value);
-        return result;
-      default:
-        return result;
-    }
-  }
-
-  // 获取查看详情/编辑详情
   async getDetail() {
     try {
       const res = await request(`/sysRole/detail/${this.roleId}`, {
@@ -83,18 +77,13 @@ class GroupDetail extends Component {
             passwordDiffer
           }
         });
-        // 获取数据库中为true的value组成的数组
-        this.oldPermissionAllTrue = this.getTrueValue(
-          this.state.permissions,
-          "permissions"
-        );
         this.oldAppAllTrue = this.getTrueValue(
           this.state.appManagerBos,
           "appManagerBos"
         );
-        this.oldTeamVisibleTrue = this.getTrueValue(
-          this.state.teamVisible,
-          "teamVisible"
+        this.oldPermissionAllTrue = this.getTrueValue(
+          this.state.permissions,
+          "permissions"
         );
       } else {
         message.error("获取详情失败");
@@ -104,60 +93,29 @@ class GroupDetail extends Component {
     }
   }
 
-  // 通过用户操作修改state
-  onChange(state, type) {
-    if (type === "getBasicInfo") {
-      this.setState({
-        baseInfoBo: {
-          ...this.state.baseInfoBo,
-          roleName: state
+  getTrueValue(state, module) {
+    const result = [];
+    switch (module) {
+      case "appManagerBos":
+        state.filter(item => item.checked).map(v => result.push(v.value));
+        return result;
+      case "permissions":
+        const { appVisible, teamVisible, teamPermission, ...rest } = state;
+        teamVisible.checked && result.push(teamVisible.value);
+        teamPermission.checked && result.push(teamPermission.value);
+        appVisible.checked && result.push(appVisible.value);
+        for (let key in rest) {
+          state[key]
+            .filter(item => item.checked)
+            .map(v => result.push(v.value));
         }
-      });
+        return result;
+      default:
+        return result;
     }
-    if (type === "getAppManage") {
-      const { checkedValue, appId } = state;
-      this.setState({
-        appManagerBos: this.state.appManagerBos.map(item => {
-          if (item.appId === appId) {
-            return {
-              ...item,
-              checked: checkedValue
-            };
-          }
-          return item;
-        })
-      });
-    }
-    if (type === "getOtherManage") {
-      const { checkedValue, key } = state;
-      const clickLine = this.state.permissions[key];
-      this.setState({
-        permissions: {
-          ...this.state.permissions,
-          [key]: clickLine.map(item => ({
-            ...item,
-            checked: checkedValue.includes(item.value)
-          }))
-        }
-      });
-    }
-  }
-
-  inputEdit(edit) {
-    this.setState({
-      edit
-    });
   }
 
   async handleDetail() {
-    // 此刻所有被选中的选项值
-    const permissionAllTrue = this.getTrueValue(
-      this.state.permissions,
-      "permissions"
-    );
-    const permissionTrueToFalse = this.oldPermissionAllTrue.filter(
-      item => !permissionAllTrue.includes(item)
-    );
     const appAllTrue = this.getTrueValue(
       this.state.appManagerBos,
       "appManagerBos"
@@ -165,25 +123,21 @@ class GroupDetail extends Component {
     const appTrueToFalse = this.oldAppAllTrue.filter(
       item => !appAllTrue.includes(item)
     );
-    const teamVisibleTrue = this.getTrueValue(
-      this.state.teamVisible,
-      "teamVisible"
+    const permissionAllTrue = this.getTrueValue(
+      this.state.permissions,
+      "permissions"
     );
-    const teamVisibleTrueToFalse = this.oldTeamVisibleTrue.filter(
-      item => !teamVisibleTrue.includes(item)
+    const permissionTrueToFalse = this.oldPermissionAllTrue.filter(
+      item => !permissionAllTrue.includes(item)
     );
 
-    // 传给后台的data数据
-    let data = {
+    const data = {
       roleId: this.roleId,
       roleName: this.state.baseInfoBo.roleName,
       appAllTrue,
       appTrueToFalse,
-      permissionAllTrue: [...permissionAllTrue, ...teamVisibleTrue],
-      permissionTrueToFalse: [
-        ...permissionTrueToFalse,
-        ...teamVisibleTrueToFalse
-      ],
+      permissionAllTrue,
+      permissionTrueToFalse,
       ...this.state.setting
     };
     try {
@@ -203,55 +157,92 @@ class GroupDetail extends Component {
     }
   }
 
-  render() {
-    // 获取是查看还是编辑行为
-    const action = this.action;
-    const { enterDetail, enterPermission } = this.props;
-    const { baseInfoBo, permissions, appManagerBos, edit } = this.state;
+  onChange(changeValue, module) {
+    if (module === "baseInfoBo") {
+      this.setState({
+        baseInfoBo: {
+          ...this.state.baseInfoBo,
+          roleName: changeValue
+        }
+      });
+    }
+    if (module === "appManagerBos") {
+      const { checkedValue, appId } = changeValue;
+      this.setState({
+        appManagerBos: this.state.appManagerBos.map(i => {
+          if (i.appId === appId) return { ...i, checked: checkedValue };
+          return i;
+        })
+      });
+    }
+    if (module === "permissions_radio") {
+      const { checkedValue, arrayItem } = changeValue;
+      this.setState({
+        permissions: {
+          ...this.state.permissions,
+          [arrayItem]: {
+            ...this.state.permissions[arrayItem],
+            checked: checkedValue
+          }
+        }
+      });
+    }
+    if (module === "permissions_checkbox") {
+      const { checkedValue, arrayItem, label } = changeValue;
+      const clickLine = this.state.permissions[arrayItem];
+      this.setState({
+        permissions: {
+          ...this.state.permissions,
+          [arrayItem]: clickLine.map(item => {
+            if (item.label === label) {
+              return {
+                ...item,
+                checked: checkedValue
+              };
+            }
+            return item;
+          })
+        }
+      });
+    }
+  }
 
+  render() {
+    const { action, enterDetail, enterPermission } = this.props;
+    const { editable, baseInfoBo, appManagerBos, permissions } = this.state;
+    const disabled = action === "view" ? true : false;
     return (
       <>
-        <Icon type="arrow-left" onClick={() => enterDetail(false)} />
-        <span style={{ textIndent: "10px" }}>分组</span>
-        {action === "edit" && (
-          <>
-            <Button
-              type="primary"
-              onClick={() => this.handleDetail()}
-              style={{ color: "#fff" }}
-            >
-              保存
-            </Button>
-            <Button onClick={() => enterDetail(false)}>取消</Button>
-          </>
-        )}
-        {getBasicInfo(action, baseInfoBo, this.onChange, edit, this.inputEdit)}
-        {getAppManage(action, appManagerBos, this.onChange, enterPermission)}
-        <div>
-          <span className={classes.moduleTitle}>
-            {this.state.teamVisible.label}
-          </span>
-          <Checkbox
-            disabled={action === "view"}
-            checked={this.state.teamVisible.checked}
-            onChange={e => {
-              this.setState({
-                teamVisible: {
-                  ...this.state.teamVisible,
-                  checked: e.target.checked
-                }
-              });
-            }}
-          ></Checkbox>
-        </div>
-        {this.state.teamVisible.checked &&
-          getOtherManage(action, permissions, this.onChange)}
-        {/* {getSetting(action, setting)} */}
+        <Top
+          disabled={disabled}
+          enterDetail={enterDetail}
+          handleDetail={this.handleDetail.bind(this)}
+        />
+        <BaseInfoModule
+          disabled={disabled}
+          baseInfoBo={baseInfoBo}
+          editable={editable}
+          setEditable={v => this.setState({ editable: v })}
+          onChange={this.onChange}
+        />
+        <AppManagerModule
+          disabled={disabled}
+          appManagerBos={appManagerBos}
+          onChange={this.onChange}
+          enterPermission={enterPermission}
+          permissions={permissions}
+        />
+        <PermissionsModule
+          disabled={disabled}
+          permissions={permissions}
+          onChange={this.onChange}
+        />
+        {/* <SettingModule disabled={disabled} settings={settings} /> */}
       </>
     );
   }
 }
+
 export default connect(({ login }) => ({
-  teamId: login.currentTeam.id,
   userId: login.userDetail.id
-}))(GroupDetail);
+}))(GroupDetail1);
