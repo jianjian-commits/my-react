5319cff639e071494c51c9280e026636506da6fb
Cindy <guohua@wctsoft.com>
2020-03-23 20:19:01 +0800

    feat 联调审批待办的个数

--- src/components/approval/index.js
+++ src/components/approval/index.js
@@ -1,6 +1,7 @@
-import React from "react";
-import { List, Icon } from "antd";
-import { useHistory } from "react-router-dom";
+import React, { useEffect } from "react";
+import { List, Icon, message } from "antd";
+import { useHistory, useParams } from "react-router-dom";
+import request from "../../utils/request"
 import {  HandledIcon, PendingIcon, SubmittedIcon } from './svg/index'
 import classes from "./approval.module.scss";
 
@@ -15,11 +16,38 @@ const baseUrl = path => {
 };
 export const ApprovalSection = props => {
   const history = useHistory();
+  const appId = useParams().appId;
+  const [todos, setTodos] = React.useState(0);
+  const [submits, setSubmits] = React.useState(0);
+  const [dones, setDones] = React.useState(0);
+  useEffect(()=>{
+    getTagsCount()
+  },[todos,submits,dones])
+  async function getTagsCount() {
+    try {
+      const res = await request(`/flow/history/approval/count`,{
+        headers:{
+          appid: appId
+        },
+      });
+      if (res && res.status === "SUCCESS") {
+        const {todos, submits, dones} = res.data;
+        setDones(dones);
+        setSubmits(submits);
+        setTodos(todos);
+      } else {
+        message.error("获取列表个数失败");
+      }
+    } catch (err) {
+      message.error("获取列表个数失败");
+    }
+  }
   const items = history => [
     {
       key: "myPending",
       icon: <PendingIcon style={{marginRight: 5}}/>,
       label: "我的待办",
+      tagNumber: todos,
       onClick: () => {
         history.push(`${baseUrl(history.location.pathname)}/myPending`);
         props.fn("myPending");
@@ -29,6 +57,7 @@ export const ApprovalSection = props => {
       key: "mySubmitted",
       icon: <SubmittedIcon style={{marginRight: 5}}/>,
       label: "我发起的",
+      tagNumber: submits,
       onClick: () => {
         history.push(`${baseUrl(history.location.pathname)}/mySubmitted`);
         props.fn("mySubmitted");
@@ -38,6 +67,7 @@ export const ApprovalSection = props => {
       key: "myHandled",
       icon: <HandledIcon style={{marginRight: 5}}/>,
       label: "我处理的",
+      tagNumber: dones,
       onClick: () => {
         history.push(`${baseUrl(history.location.pathname)}/myHandled`);
         props.fn("myHandled");
@@ -60,7 +90,7 @@ export const ApprovalSection = props => {
             >
             {item.icon}
             {item.label}
-            <span className={classes.tag}>2</span>
+          <span className={classes.tag}>{item.tagNumber}</span>
           </List.Item>
           )}
         ></List>
