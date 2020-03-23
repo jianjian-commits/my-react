4a6e5871918105f6389b8b48553e70b309ce05a6
ornatejester <1454030137@qq.com>
2020-03-23 17:58:33 +0800

    feat(BI) show form list

--- src/components/bi/component/dashboard/DBToolbar.js
+++ src/components/bi/component/dashboard/DBToolbar.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useState} from "react";
+import React, {Fragment, useState,useEffect} from "react";
 import { connect } from "react-redux";
 import { Button, Icon, Modal, Tooltip, Spin, message } from "antd";
 import { useParams, useHistory } from "react-router-dom";
@@ -15,40 +15,55 @@ const DBToolbar = props => {
   const { setFormData, setDataSource } = props;
   let form = {};
 
-  const newChart = () => {
+  // const newChart = () => {
+  //   const dataRes = request(`/bi/forms?appId=`, {
+  //     method: "GET",
+  //     data: {
+  //       appId
+  //     }
+  //   })
+
+  //   dataRes.then((res) => {
+  //     if(res && res.msg === 'success') {
+  //       const items = res.data.items || [];
+  //       setFormData(items);
+  //       form = items[1];
+  //       console.log(form);
+  //     }
+  //   }, () => {message.error("获取数据失败")}).then(()=> {
+  //     const res = request(`/bi/charts`, {
+  //       method: "POST",
+  //       data: {
+  //         name: "新建图表",
+  //         dashboardId,
+  //         formId: form.formId
+  //       }
+  //     }).then((res) => {
+  //       if(res && res.msg === 'success') {
+  //         const data = res.data;
+  //         const view = data.view;
+  //         const elementId = view.id;
+  //         setDataSource({id: form.formId, name: form.formName, data: view.formFields});
+  //         history.push(`/app/${form.formId}/setting/bi/${dashboardId}/${elementId}`);
+  //       }
+  //     }, () => {message.error("创建图标失败")})
+  //   })
+  // }
+
+  useEffect(() => {
     const dataRes = request(`/bi/forms?appId=`, {
       method: "GET",
       data: {
         appId
       }
     })
-
     dataRes.then((res) => {
       if(res && res.msg === 'success') {
         const items = res.data.items || [];
         setFormData(items);
-        form = items[1];
-        console.log(form);
       }
-    }, () => {message.error("获取数据失败")}).then(()=> {
-      const res = request(`/bi/charts`, {
-        method: "POST",
-        data: {
-          name: "新建图表",
-          dashboardId,
-          formId: form.formId
-        }
-      }).then((res) => {
-        if(res && res.msg === 'success') {
-          const data = res.data;
-          const view = data.view;
-          const elementId = view.id;
-          setDataSource({id: form.formId, name: form.formName, data: view.formFields});
-          history.push(`/app/${form.formId}/setting/bi/${dashboardId}/${elementId}`);
-        }
-      }, () => {message.error("创建图标失败")})
-    })
-  }
+    }, () => {message.error("获取数据失败")})
+  },[]);
 
   const [visible, setVisible] = useState(false);
   const modalProps = {
@@ -70,7 +85,7 @@ const DBToolbar = props => {
       <Icon
         type="plus-circle"
         className="new-chart-icon"
-        onClick={newChart}
+        onClick={modalProps.showModal}
       />
       <div className="new-chart" onClick={modalProps.showModal}>新建图表</div>
     </div>
