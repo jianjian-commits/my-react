2c6a7dc666dc7db6650d5fd33d23cc51bca3f4f4
maochao <maochao@wctsoft.com>
2020-03-25 16:26:37 +0800

    ui调整

--- src/components/userManagement/team/TeamInfo.js
+++ src/components/userManagement/team/TeamInfo.js
@@ -4,7 +4,7 @@ import { Input, Row, Col, List, Button, Spin, message, Popover } from "antd";
 import request from "../../../utils/request";
 import classes from "./team.module.scss";
 import { getCurrentTeam, getAllTeam } from "../../../store/loginReducer";
-import { ReactComponent as Edit } from "../../../styles/images/edit.svg";
+import { ReactComponent as Edit } from "../../../assets/icons/edit.svg";
 import Authenticate from "../../shared/Authenticate";
 import { TEAM_MANAGEMENT_UPDATE_INFO } from "../../../auth";
 
