721d5a38b441f718311e3e50cd0c0aa706f3e2a2
Cindy <guohua@wctsoft.com>
2020-03-27 19:22:50 +0800

    移除多余的代码。。

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,9 +1,3 @@
-export { ReactComponent as CreateIcon } from "./teams/create.svg";
 export { ReactComponent as EditIcon } from "./edit.svg";
-export { ReactComponent as PromptIcon } from "./prompt.svg";
-export { ReactComponent as ArrowLeftIcon } from "./arrow-left.svg";
-export { ReactComponent as CloseIcon } from "./close.svg";
 export { ReactComponent as TableIcon } from "./table.svg";
-export { ReactComponent as RightOutlined } from "./rightOutlined.svg";
-export { ReactComponent as WarningIcon } from "./warning.svg";
-export { ReactComponent as DightOutlined } from "./downOutlined.svg";
+
