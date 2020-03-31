77e1631cf972abeff07e955a5914ab17bb99f12d
ornatejester <1454030137@qq.com>
2020-03-31 14:25:37 +0800

    fix(BI) 修改指标下拉框样式

--- src/components/bi/component/elements/modal/changeTipModal.js
+++ src/components/bi/component/elements/modal/changeTipModal.js
@@ -6,7 +6,7 @@ const content = "更改数据源将清空当前图表配置，是否确认修改
 
 export function ModalTitle(props) {
   return (
-    <div className="warningModalTitle">
+    <div className="normalModalTitle">
       <Icon type="exclamation-circle" theme="filled" />
       {props.children}
     </div>
@@ -26,11 +26,11 @@ export default function ExitWaringTip(props) {
       centered
       handleCancel={props.handleCancel}
     >
-      <div className="warningModalContainer">
-        <div className="warningModalContent">{content}</div>
-        <div className="warningBtnGroups">
+      <div className="normalModalContainer">
+        <div className="normalModalContent">{content}</div>
+        <div className="warningBtnGroup">
           <Button onClick={props.handleCancel}>取消</Button>
-          <Button>确定</Button>
+          <Button onClick={props.handleOk}>确定</Button>
         </div>
       </div>
     </Modal>
