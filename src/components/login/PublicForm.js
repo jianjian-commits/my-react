5b777b4f1e9aa51127eaa0bdde83e83c88503796
maochao <maochao@wctsoft.com>
2020-04-01 12:11:54 +0800

    ui问题

--- src/components/login/PublicForm.js
+++ src/components/login/PublicForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Form } from "antd";
-import { formItems } from "./FormItemConfig";
+import { formItems } from "./formItemConfig";
 
 export default Form.create({ name: "login-form" })(function PublicForm({
   form,
