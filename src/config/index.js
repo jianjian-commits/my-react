e5395009fdfd4e7d86a19e34238e47a3bd9e3b89
liuyang <liuyang@wctsoft.com>
2020-04-11 17:44:13 +0800

    样式调整

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const HOST_IP = "http://192.168.3.105:9080" || process.env.REACT_APP_HOST_IP;
+export const HOST_IP = "" || process.env.REACT_APP_HOST_IP;
 export const env = "REMOTE"; // REMOTE/LOCAL
