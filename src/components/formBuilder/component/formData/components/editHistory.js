53d0c52d453fbaee1f705d34c8aeeb7b88b0e99f
KomonWorker <1126972456@qq.com>
2020-04-15 10:54:43 +0800

    feat(编辑记录)提交时为子表单数据添加唯一ID

--- src/components/formBuilder/component/formData/components/editHistory.js
+++ src/components/formBuilder/component/formData/components/editHistory.js
@@ -2,7 +2,7 @@
  * @Author: your name
  * @Date: 2020-04-10 15:15:00
  * @LastEditors: komons
- * @LastEditTime: 2020-04-15 09:36:26
+ * @LastEditTime: 2020-04-15 10:40:57
  * @Description:
  * @FilePath: \form-builderc:\Komons\work\all\davinci-paas-frontend\src\components\formBuilder\component\formData\components\editHistory.js
  */
@@ -44,9 +44,22 @@ const EditHistory = ({ submissionId }) => {
   const [historyData, setHistoryData] = useState([]);
   const [isLoading, setLoadingStatus] = useState(true);
 
+  const filterHistryDom = arr => {
+    if (arr) {
+      let res = arr.map(item => (
+        <p>
+          修改{item.label}的值{item.beforeValue}为{item.afterValue}
+        </p>
+      ));
+      return <div>{res}</div>;
+    } else {
+      return "创建记录";
+    }
+  };
+
   useEffect(() => {
     axios({
-      url: config.apiUrl + `/submission/update/record`,
+      url: config.apiUrl + `/update_record`,
       method: "get",
       params: {
         submissionId
@@ -59,14 +72,21 @@ const EditHistory = ({ submissionId }) => {
       setHistoryData(
         res.data.map(item => ({
           editor: item.extraProp.updateUser,
-          date: (new Date(item.updateTime)).toLocaleString(),
-        content: <div>{item.updateDescription && item.updateDescription.split("\n").map(item => (<>{item}<br /></>))}</div>,
+          date: new Date(item.updateTime).toLocaleString(),
+          content: filterHistryDom(item.changeFieldUpdateRecords),
           key: item.updateTime
         }))
       );
     });
   }, [submissionId]);
-  return <Table loading={isLoading} columns={columns} dataSource={historyData} pagination={paginationProps}></Table>;
+  return (
+    <Table
+      loading={isLoading}
+      columns={columns}
+      dataSource={historyData}
+      pagination={paginationProps}
+    ></Table>
+  );
 };
 
 export default EditHistory;
--- src/components/formBuilder/component/submission/submission.js
+++ src/components/formBuilder/component/submission/submission.js
@@ -58,6 +58,8 @@ import moment from "moment";
 import PureTime from "./component/pureTime";
 import PureDate from "./component/pureDate";
 
+import ID from "../../utils/UUID";
+
 function hasErrors(fieldsError) {
   return Object.keys(fieldsError).some(field => fieldsError[field]);
 }
@@ -246,7 +248,7 @@ class Submission extends Component {
           delete values[component.key];
         } else {
           values[component.key].xx = address;
-          console.log("values[component.key]",values[component.id])
+          console.log("values[component.key]", values[component.id]);
         }
       }
     });
@@ -324,15 +326,16 @@ class Submission extends Component {
       }
     }
   }
-  _resetFormChildErrorMsg(errorMsg){
+
+  _resetFormChildErrorMsg(errorMsg) {
     const { formChildDataObj } = this.state;
-    const {formChildkey, fieldKey, value, msg} = errorMsg;
+    const { formChildkey, fieldKey, value, msg } = errorMsg;
     formChildDataObj[formChildkey].map(item => {
-        if (value === String(item[fieldKey].data)) {
-          console.log("item", item[fieldKey])
-          item[fieldKey].hasErr = true;
-        }
-      });
+      if (value === String(item[fieldKey].data)) {
+        console.log("item", item[fieldKey]);
+        item[fieldKey].hasErr = true;
+      }
+    });
     this.setState({
       showFormChildErr: true
     });
@@ -430,20 +433,30 @@ class Submission extends Component {
   _setErrorResponseData = errorResponseData => {
     let errorResponseMsg = {};
     errorResponseData.infos.map(info => {
-      if(info.fieldName.indexOf(".") !== -1){
+      if (info.fieldName.indexOf(".") !== -1) {
         // 子表单
         // 子表单id.组件id.组件的值
         // arr[0]表示子表单id,arr[1]表示组件id,arr[2]表示组件的值
         const arr = info.fieldName.split(".");
-        var value = info.fieldName.substring(arr[0].length+arr[1].length+2); 
+        var value = info.fieldName.substring(arr[0].length + arr[1].length + 2);
         console.log("value", value);
         //  这里有问题最后的值不能用.分割
-        const infoMsg = {formChildkey:arr[0], fieldKey: arr[1], value: value, msg: info.msg};
+        const infoMsg = {
+          formChildkey: arr[0],
+          fieldKey: arr[1],
+          value: value,
+          msg: info.msg
+        };
         this._resetFormChildErrorMsg(infoMsg);
-        if(errorResponseMsg[infoMsg.formChildkey] != void 0 && errorResponseMsg[infoMsg.formChildkey][infoMsg.fieldKey] != void 0){
-          errorResponseMsg[infoMsg.formChildkey][infoMsg.fieldKey].push(infoMsg);
-        } else if(errorResponseMsg[infoMsg.formChildkey] == void 0){
-          errorResponseMsg[infoMsg.formChildkey]={};
+        if (
+          errorResponseMsg[infoMsg.formChildkey] != void 0 &&
+          errorResponseMsg[infoMsg.formChildkey][infoMsg.fieldKey] != void 0
+        ) {
+          errorResponseMsg[infoMsg.formChildkey][infoMsg.fieldKey].push(
+            infoMsg
+          );
+        } else if (errorResponseMsg[infoMsg.formChildkey] == void 0) {
+          errorResponseMsg[infoMsg.formChildkey] = {};
           errorResponseMsg[infoMsg.formChildkey][infoMsg.fieldKey] = [infoMsg];
         } else {
           errorResponseMsg[infoMsg.formChildkey][infoMsg.fieldKey] = [infoMsg];
@@ -452,7 +465,7 @@ class Submission extends Component {
         // 普通组件
         if (errorResponseMsg[info.fieldName] != void 0) {
           errorResponseMsg[info.fieldName].push(info.msg);
-        } else{
+        } else {
           errorResponseMsg[info.fieldName] = [info.msg];
         }
       }
@@ -465,7 +478,7 @@ class Submission extends Component {
 
   // 设置正确的子表单数据
   setCorrectFormChildData = (values, formChildDataObj) => {
-    let date = new Date((new Date()).setUTCMilliseconds(0));
+    let date = new Date(new Date().setUTCMilliseconds(0));
     let currentTimeZoneOffsetInHours = date.getTimezoneOffset() / 60;
     date.setHours(date.getHours() + currentTimeZoneOffsetInHours);
 
@@ -475,6 +488,10 @@ class Submission extends Component {
       }
       if (Array.isArray(values[key])) {
         values[key].forEach((data, index) => {
+          console.log("data", data);
+          if (!data["childFormDataId"]) {
+            data["childFormDataId"] = ID.oldUuid();
+          }
           for (let k in data) {
             let type = data[k].formType;
             if (data[k].autoInput) {
@@ -546,11 +563,11 @@ class Submission extends Component {
         values = this._setDateTimeVaule(values);
         values = this._setAddressValue(values);
         this.setCorrectFormChildData(values, this.state.formChildDataObj);
-        this._iterateAllComponentToSetData(
-          formComponentArray,
-          customDataArray,
-          values
-        );
+        // this._iterateAllComponentToSetData(
+        //   formComponentArray,
+        //   customDataArray,
+        //   values
+        // );
 
         let customValicate = this.props.formValidation;
         // console.log("values", values);
