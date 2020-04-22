039f836db994f97b931e0dcb1a67a8cc7e5fead2
weiliue <2421535416@qq.com>
2020-04-22 14:56:04 +0800

    update style

--- src/components/bi/component/elements/ElemType.js
+++ src/components/bi/component/elements/ElemType.js
@@ -1,5 +1,6 @@
 export const chartGroup = [
-  {type:"INDEX_DIAGRAM",intro:"指标图 1个维度,1个指标 0个维度,1个指标",available:true},
-  {type:"AREA_CHART",intro:"饼状图 1个维度,1个指标",available:true},
-  {type:"HISTOGRAM",intro:"柱状图 1个维度,2个或多个指标 2个维度,1个指标",available:true}
+  {type:"INDEX_DIAGRAM",intro:["指标图","0个维度，1个指标","1个维度，1个指标"],available:true},
+  {type:"AREA_CHART",intro:["饼状图","1个维度，1个指标"],available:true},
+  {type:"HISTOGRAM",intro:["柱形图","1个维度，1个或多个指标","2个维度，1个指标"],available:true}
 ]
+
