62ab58edde12293197e9f20c6bcd8f3b6da3eee4
weiliue <2421535416@qq.com>
2020-04-24 15:18:35 +0800

    change icon style

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,4 +1,5 @@
 export { ReactComponent as EditIcon } from "./edit.svg";
 export { ReactComponent as BreadRight} from "./bread-right.svg";
 export { ReactComponent as TableIcon } from "./table.svg";
+export { ReactComponent as DashboardIcon } from "./dashboard.svg";
 
